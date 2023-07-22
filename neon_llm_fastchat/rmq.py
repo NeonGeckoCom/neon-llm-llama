525455af424d00a42127196cb1fd70245c6b76a1
NeonBohdan <bohdan@neon.ai>
2023-07-22 22:01:48 +0300

    Added score mq handler

--- neon_llm_fastchat/rmq.py
+++ neon_llm_fastchat/rmq.py
@@ -93,3 +93,39 @@ class FastchatMQ(MQConnector):
                                   expiration=str(1000)))
         channel.basic_ack(method.delivery_tag)
         LOG.info(f"Handled request: {message_id}")
+
+    @create_mq_callback(include_callback_props=('channel', 'method', 'body'))
+    def handle_score_request(self,
+                       channel: pika.channel.Channel,
+                       method: pika.spec.Basic.Return,
+                       body: dict):
+        """
+        Handles score requests from MQ to FastChat received on queue
+        "request_fastchat"
+
+        :param channel: MQ channel object (pika.channel.Channel)
+        :param method: MQ return method (pika.spec.Basic.Return)
+        :param body: request body (dict)
+        """
+        message_id = body["message_id"]
+        routing_key = body["routing_key"]
+
+        query = body["query"]
+        responses = body["responses"]
+
+        ppl_scores = self.fastChat.ppl(question=query, answers=responses)
+        best_id = self.fastChat.get_best(ppl_scores)
+
+        api_response = {
+            "message_id": message_id,
+            "ppl_scores": ppl_scores,
+            "best": best_id
+        }
+
+        channel.basic_publish(exchange='',
+                              routing_key=routing_key,
+                              body=dict_to_b64(api_response),
+                              properties=pika.BasicProperties(
+                                  expiration=str(1000)))
+        channel.basic_ack(method.delivery_tag)
+        LOG.info(f"Handled score request: {message_id}")
