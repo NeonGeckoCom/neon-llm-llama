c40d2f22a94630a8fdcb1011d4e32c2d367ea112
NeonBohdan <bohdan@neon.ai>
2023-08-28 18:19:53 +0300

    Remove duplicating BOS token

--- neon_llm_llama/llama.py
+++ neon_llm_llama/llama.py
@@ -73,7 +73,7 @@ class Llama(NeonLLM):
 
     @property
     def _system_prompt(self) -> str:
-        return ("<s>[INST] <<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. "
+        return ("[INST] <<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. "
         "Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. "
         "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
         "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. "
